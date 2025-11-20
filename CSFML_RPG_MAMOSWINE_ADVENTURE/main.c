03a7e8adfa36f11c10c9d20a575e1d8f52c1a220
BlashOwO <mathdu30400@gmail.com>
2025-11-20 09:59:33 +0100

    map du jeu

---
+++ CSFML_RPG_MAMOSWINE_ADVENTURE/main.c
@@ -0,0 +1,46 @@
+#include "SFML/Graphics.h"
+#include "tools.h"
+#include "map.h"
+int main()
+{
+
+	initAll();
+
+	sfVideoMode mode = { 800,600,32 };
+
+	sfRenderWindow* window = sfRenderWindow_create(mode, "Test", sfClose, NULL);
+
+	sfEvent event;
+	
+	while (sfRenderWindow_isOpen(window))
+	{
+	
+		restartClock();
+		while (sfRenderWindow_pollEvent(window, &event))
+		{
+			if (event.type == sfEvtClosed)
+			{
+				sfRenderWindow_close(window);
+			}
+			if (event.type == sfEvtMouseButtonPressed)
+			{
+				pressed = 1;
+			}
+			else if (event.type == sfEvtMouseButtonReleased)
+			{
+				pressed = 0;
+			}
+		}
+
+		if (state == MENU)
+		{
+			sfRenderWindow_setMouseCursorVisible(window, sfTrue);
+		}
+		updateAll(window);
+		sfRenderWindow_clear(window, sfBlack);
+		displayAll(window);
+
+		sfRenderWindow_display(window);
+	}
+
+}
---
+++ CSFML_RPG_MAMOSWINE_ADVENTURE/tools.c
@@ -0,0 +1,75 @@
+#include "tools.h"
+sfTime sftime;
+sfClock* sfclock;
+
+
+
+void initTools()
+{
+	sfclock = sfClock_create();
+	srand(time(NULL));
+}
+
+void restartClock()
+{
+	sftime = sfClock_restart(sfclock);
+}
+
+float GetDeltaTime()
+{
+	return sfTime_asSeconds(sftime);
+}
+int isInside(sfVector2f item, sfFloatRect obstacle)
+{
+	if (item.x > obstacle.left &&
+		item.x < (obstacle.width + obstacle.left) &&
+		item.y > obstacle.top &&
+		item.y < (obstacle.top + obstacle.height))
+		{
+			return 1;
+
+}
+return 0;
+}
+
+
+void initAll()
+{
+	initTools();
+
+
+
+	initMusic();
+
+	initmap();
+
+	initView();
+
+	initplayer();
+
+	initNPC();
+
+	initTextBox();
+	
+	initmenu();
+	
+}
+
+void updateAll(sfRenderWindow* _window)
+{
+
+	updatemenu(_window);
+	updateViewEditor(_window);
+	updatePlayer(_window);
+	updateTextBox();
+	updateMap(_window);
+}
+void displayAll(sfRenderWindow* _window)
+{
+	displayMap(_window);
+	displayView(_window);
+	displayPlayer(_window);
+	displayNPC(_window);
+	displaymenu(_window);
+	//displayTextBox(_window);
+}
